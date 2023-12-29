dc6d08b3e65856436c4ef2b2e74af5e003551387
Hritik Raj <hritik@Hritik-2.local>
2023-12-29 23:11:48 +0530

    home page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-
+import {BrowserRouter, Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <BroswerRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            />
+          </Routes>
+        </div>
       
+      
+      
+      </BroswerRouter>
     </div>
   );
 }
